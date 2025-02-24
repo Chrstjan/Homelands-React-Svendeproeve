bbe635e6f35445a0169d08abf8480b041cb8fccc
Chrstjan <ps5christian@hotmail.com>
2025-02-24 09:46:45 +0100

    #1 #2 #5 #6 #7 #17 #18 made project base setup

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { BrowserRouter as Router } from "react-router-dom";
+import { PageRouter } from "./router/PageRouter";
+import './App.scss'
+
+function App() {
+
+  return (
+    <>
+      <Router>
+        <PageRouter />
+      </Router>
+    </>
+  )
+}
+
+export default App
